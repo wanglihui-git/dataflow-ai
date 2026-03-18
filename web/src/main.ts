0a20dfa2b85f7a09d68b2eecc876180ff486b8cd
王理晖 <949764788@qq.com>
2026-03-18 19:00:54 +0800

    1.界面初始化

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import zhCn from "element-plus/dist/locale/zh-cn.mjs"
+import zhCn from "element-plus/es/locale/lang/zh-cn"
 
 import App from './App.vue'
 import router from './router'
